51da65dda84ce6aee72eb51cd68ee04fd089698e
Kevin Berry <kberry@geoforce.com>
2014-01-21 10:27:07 -0600

    Add attribute and template food for erroronly notifications.  Resolves #1

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,6 @@ default['unattended-upgrades']['upgrade_interval'] = "1"
 default['unattended-upgrades']['origins'] = ['${distro_id} ${distro_codename}-security']
 default['unattended-upgrades']['send_email'] = false
 default['unattended-upgrades']['email_address'] = "root@localhost"
+default['unattended-upgrades']['mail_only_on_error'] = false
 default['unattended-upgrades']['auto_remove'] = false
 default['unattended-upgrades']['auto_reboot'] = false
